04d18a235e78b3d1d091cbde23ee5409c25f9260
thestartitan <ankitAryamanSharma@gmail.com>
2026-03-08 11:47:23 +0530

    B: limit game rendering to 60 FPS

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -13,6 +15,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
