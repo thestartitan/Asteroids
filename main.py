a84ece7ea74cff582271499372e1e19d0ebbae25
thestartitan <ankitAryamanSharma@gmail.com>
2026-03-10 10:33:17 +0530

    D: add movement functionality

--- main.py
+++ main.py
@@ -8,20 +8,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
